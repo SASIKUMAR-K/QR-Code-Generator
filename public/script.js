0cdebbe7df798349781e9f42d2e0a9fe3f467c74
SASIKUMAR-K <sasikumar05112004@gmail.com>
2024-06-18 12:50:33 +0530

    Copyright, Header, Hex Input Added

--- public/script.js
+++ public/script.js
@@ -14,11 +14,11 @@ function generateQRCode(e) {
 	if (url === '') {
 		alert('Please Enter Your Website Link');
 	} else {
-		//Show Spinner
+		// Show Spinner
 		spinner.style.display = 'flex';
 
 		setTimeout(() => {
-			//Hide Spinner
+			// Hide Spinner
 			spinner.style.display = 'none';
 			qrcodeElement.innerHTML = '';
 
@@ -35,7 +35,48 @@ function generateQRCode(e) {
 		createDownloadLink();
 	}
 }
+
 frm.addEventListener('submit', generateQRCode);
+
+// Update color picker value when hex input changes for dark color
+const hexDarkInput = document.querySelector('#hexDark');
+const colorDarkInput = document.querySelector('#colorDark');
+
+hexDarkInput.addEventListener('input', () => {
+	const hexCode = hexDarkInput.value.trim().replace(/^#/, '');
+	if (hexCode.length === 6 || hexCode.length === 3) {
+		const hexColor = '#' + hexCode;
+		colorDarkInput.value = hexColor;
+	}
+});
+
+// Update hex input value when color picker changes for dark color
+colorDarkInput.addEventListener('input', () => {
+	const hexCode = colorDarkInput.value.trim().replace(/^#/, '');
+	if (hexCode.length === 6 || hexCode.length === 3) {
+		hexDarkInput.value = hexCode.toUpperCase();
+	}
+});
+
+// Update color picker value when hex input changes for light color
+const hexLightInput = document.querySelector('#hexLight');
+const colorLightInput = document.querySelector('#colorLight');
+
+hexLightInput.addEventListener('input', () => {
+	const hexCode = hexLightInput.value.trim().replace(/^#/, '');
+	if (hexCode.length === 6 || hexCode.length === 3) {
+		const hexColor = '#' + hexCode;
+		colorLightInput.value = hexColor;
+	}
+});
+
+// Update hex input value when color picker changes for light color
+colorLightInput.addEventListener('input', () => {
+	const hexCode = colorLightInput.value.trim().replace(/^#/, '');
+	if (hexCode.length === 6 || hexCode.length === 3) {
+		hexLightInput.value = hexCode.toUpperCase();
+	}
+});
 
 function createDownloadLink() {
 	const imgSrc = qrcodeElement.querySelector('img').src;
